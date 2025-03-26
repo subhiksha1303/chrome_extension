591c66abad399bace0e37a01bcf86beabfc00340
Subhiksha.K <subhiksha13@SubhikshaKs-MacBook-Air.local>
2025-03-26 11:36:03 +0530

    Removed python from requirements.txt and added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bias_checker.wsgi:application --timeout 120 --workers 3
--- biasbuster_chrome_extension/settings.py
+++ biasbuster_chrome_extension/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,6 +90,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "USER": "root",
+        "PASSWORD": "Subhiksha13",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
