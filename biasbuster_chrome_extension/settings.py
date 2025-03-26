98030f5781e7190c9944b890eb1232758dc42382
Subhiksha.K <subhiksha13@SubhikshaKs-MacBook-Air.local>
2025-03-26 12:09:58 +0530

    Fixed ALLOWED_HOSTS issue

--- biasbuster_chrome_extension/settings.py
+++ biasbuster_chrome_extension/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-veatw5$juchtl2!l6ze0dt&y=e7$)19-yemgu17c(w%4qqficn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://chrome-extension-d6vc.onrender.com"]
+ALLOWED_HOSTS = ["chrome-extension-d6vc.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
